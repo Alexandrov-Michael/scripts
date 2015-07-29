229be1d4c4525a7bb1f09eb35a829824e676e64c
Alexandrov-Michael <mikle.alex@gmail.com>
2015-07-29 10:58:34 +0300

    change backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
-#/bin/bash
+#!/bin/bash
 # Backup scripst for postgressql bases
 
 bases=('pa-t' 'complex' 'pa-soft' 'pa-invest')
 mountpoint="//backup-srv/share/psql"
 
+mountdir="/mnt/psql/"
 testmount=$(/bin/mount | grep $mountpoint)
 datetime=$(/bin/date +%Y-%m-%d_%H-%M-%S)
 PROGNAME=$(/usr/bin/basename $0)
@@ -18,9 +19,10 @@ then
 	for b in ${bases[@]}
 	do
 		/usr/bin/pg_dump -U postgres $b | /bin/gzip > /mnt/psql/$b.$datetime.pgsql.gz
+		ls -1r $mountdir | grep $b | sed -n '10,$ p' | xargs rm -f {}
 	done
 else
-	error_exit "Folder PSQL not mounted"
+	error_exit "$datetime: Folder PSQL not mounted"
 fi
 
 exit 0
