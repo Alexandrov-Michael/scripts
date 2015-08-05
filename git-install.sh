c2f5972d9b1d661b811c816cfaf7644ee0b9ed36
Alexandrov-Michael <mikle.alex@gmail.com>
2015-08-05 10:56:03 +0300

    add git-install.sh

---
+++ git-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev git
+git config --global user.name "Alexandrov-Michael"
+git config --global user.email "mikle.alex@gmail.com"
