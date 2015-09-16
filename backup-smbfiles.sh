ef526d7c54c07e10ef9db1ad4897296a01a76e2a
Alexandrov-Michael <mikle.alex@gmail.com>
2015-09-16 13:08:59 +0300

    change backup-smb

--- backup-smbfiles.sh
+++ backup-smbfiles.sh
@@ -4,17 +4,24 @@
 dir-s="/mnt/Projects"
 dir-d="/data/Projects/latest"
 dir-date="projects-$(date +%Y-%m-%d_%H-%M-%S)"
-mountpoint="//ns/Проекты"
-testmount=$(mount | grep $mountpoint)
+mountpoint1="//ns/Проекты"
+mountpoint2="/dev/sda1"
+testmount1=$(mount | grep $mountpoint1)
+testmount2=$(mount | grep $mountpoint2)
 PROGNAME=$(basename $0)
 
 
-if [ -n "$testmount" ]
+if [ -n "$testmount1" ]
 then
-	rsync --archive --delete --delete-excluded $dir-s $dir-d || logger "PROBLEM: $PROGNAME rsync aborted"
-	cp --archive --link $dir-d $dir-date || logger "PROBLEM: $PROGNAME cp aborted"
+	if [ -n "$testmount2" ]
+	then   
+	        rsync --archive --delete --delete-excluded $dir-s $dir-d || logger "PROBLEM: $PROGNAME rsync aborted"
+       		cp --archive --link $dir-d $dir-date || logger "PROBLEM: $PROGNAME cp aborted"
+	else   
+        	logger "PROBLEM: $PROGNAME $mountpoint2 not mounting"
+	fi
 else
-	logger "PROBLEM: $PROGNAME $mountpoint not mounting"
+	logger "PROBLEM: $PROGNAME $mountpoint1 not mounting"
 fi
 
 exit 0
