f0fe893b4de2325d91c74ba7df670eea65290476
Alexandrov-Michael <mikle.alex@gmail.com>
2015-09-21 09:47:42 +0300

    zabbix agent install

---
+++ zabbix-agent-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#install zabbix agent debian 7
+
+wget http://repo.zabbix.com/zabbix/2.0/debian/pool/main/z/zabbix-release/zabbix-release_2.0-1wheezy_all.deb
+dpkg -i zabbix-release_2.0-1wheezy_all.deb
+apt-get update
