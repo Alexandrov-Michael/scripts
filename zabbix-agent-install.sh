09b9f4db3c4160bb2b038a2b425ad1107f0bd61c
Alexandrov-Michael <mikle.alex@gmail.com>
2015-09-21 10:01:02 +0300

    zabbix agent install zabbix restart

--- zabbix-agent-install.sh
+++ zabbix-agent-install.sh
@@ -5,3 +5,4 @@ wget http://repo.zabbix.com/zabbix/2.0/debian/pool/main/z/zabbix-release/zabbix-
 dpkg -i zabbix-release_2.0-1wheezy_all.deb
 apt-get update
 apt-get install zabbix-agent && usermod -a -G adm zabbix
+/etc/init.d/zabbix-agent restart
