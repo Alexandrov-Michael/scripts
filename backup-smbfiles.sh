8cf8e16b9be939144f1b0797d75561b7fbf7705b
Alexandrov-Michael <mikle.alex@gmail.com>
2015-09-21 10:09:19 +0300

    backup projects add zabbix info tag

--- backup-smbfiles.sh
+++ backup-smbfiles.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 #Backup files intremental
 
-dir-s="/mnt/Projects"
-dir-d="/data/Projects/latest"
-dir-date="projects-$(date +%Y-%m-%d_%H-%M-%S)"
+dirs="/mnt/Projects"
+dird="/data/Projects/latest"
+dirdate="/data/Projects/projects-$(date +%Y-%m-%d_%H-%M-%S)"
+logfile="/data/Projects/rsync.log"
 mountpoint1="//ns/Проекты"
 mountpoint2="/dev/sda1"
 testmount1=$(mount | grep $mountpoint1)
 testmount2=$(mount | grep $mountpoint2)
-PROGNAME=$(basename $0)
+PROGNAME="$(pwd -P)/$(basename $0)"
 
+logger "$PROGNAME Backup Projects is starting..."
 
 if [ -n "$testmount1" ]
 then
 	if [ -n "$testmount2" ]
 	then   
-	        rsync --archive --delete --delete-excluded $dir-s $dir-d || logger "PROBLEM: $PROGNAME rsync aborted"
-       		cp --archive --link $dir-d $dir-date || logger "PROBLEM: $PROGNAME cp aborted"
+	        rsync --archive --delete --delete-excluded $dirs $dird > $logfile || logger "PROBLEM: $PROGNAME rsync aborted"
+       		cp --archive --link $dird $dirdate || logger "PROBLEM: $PROGNAME cp aborted"
+		logger "INFO: $PROGNAME Backup Projects is complite"
 	else   
         	logger "PROBLEM: $PROGNAME $mountpoint2 not mounting"
 	fi
