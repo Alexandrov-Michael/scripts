ca8b121cc134d9f256bb7c04b4a7f99c1af76d1d
Alexandrov-Michael <mikle.alex@gmail.com>
2015-09-21 09:50:44 +0300

    zabbix agent install change

--- zabbix-agent-install.sh
+++ zabbix-agent-install.sh
@@ -4,3 +4,4 @@
 wget http://repo.zabbix.com/zabbix/2.0/debian/pool/main/z/zabbix-release/zabbix-release_2.0-1wheezy_all.deb
 dpkg -i zabbix-release_2.0-1wheezy_all.deb
 apt-get update
+apt-get install zabbix-agent
