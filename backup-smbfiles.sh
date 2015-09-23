379fa5f2b8fd01e9280a59580008a580885c493a
Alexandrov-Michael <mikle.alex@gmail.com>
2015-09-23 09:25:09 +0300

    update backup proj

--- backup-smbfiles.sh
+++ backup-smbfiles.sh
@@ -17,15 +17,8 @@ if [ -n "$testmount1" ]
 then
 	if [ -n "$testmount2" ]
 	then   
-	        rsync --archive --delete --delete-excluded $dirs $dird &> $logfile
+	        rsync --archive --delete --delete-excluded --exclude="*.lck" $dirs $dird &> $logfile || logger "PROBLEM: $PROGNAME backup projects stoped whith rsync errors" && exit 1
        		cp --archive --link $dird $dirdate || logger "PROBLEM: $PROGNAME cp aborted" && exit 1
-		rsynclog="$(cat $logfile)"
-		# провера лога rsync на ошибки
-		if [ -n "$rsynclog" ]
-		then
-			logger "PROBLEM: $PROGNAME backup projects stoped whith rsync errors"
-			exit 1
-		fi
 		logger "INFO: $PROGNAME Backup Projects is complite"
 	else   
         	logger "PROBLEM: $PROGNAME $mountpoint2 not mounting"
