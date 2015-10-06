8473ca2a33dad0493880b74cbf48e1fd31b7c7a6
Alexandrov-Michael <mikle.alex@gmail.com>
2015-10-06 09:05:15 +0300

    modified backup-smb

--- backup-smbfiles.sh
+++ backup-smbfiles.sh
@@ -11,21 +11,29 @@ testmount1=$(mount | grep $mountpoint1)
 testmount2=$(mount | grep $mountpoint2)
 PROGNAME="$(pwd -P)/$(basename $0)"
 
+function flg () {
+	logger "$1"
+	exit 1
+}
+
 logger "$PROGNAME Backup Projects is starting..."
 
 if [ -n "$testmount1" ]
 then
 	if [ -n "$testmount2" ]
 	then   
-	        rsync --archive --delete --delete-excluded --exclude="*.lck" $dirs $dird &> $logfile || logger "PROBLEM: $PROGNAME backup projects stoped whith rsync errors" && exit 1
-       		cp --archive --link $dird $dirdate || logger "PROBLEM: $PROGNAME cp aborted" && exit 1
+		logger "$PROGNAME backup projects rsync started..."
+	        rsync --archive --delete --delete-excluded --exclude="*.lck" $dirs $dird &> $logfile || flg "PROBLEM: $PROGNAME backup projects aborted with rsync error"
+		logger "$PROGNAME backup projects cp started..."
+       		cp -rl $dird $dirdate || flg "PROBLEM: $PROGNAME cp aborted"
 		logger "INFO: $PROGNAME Backup Projects is complite"
 	else   
         	logger "PROBLEM: $PROGNAME $mountpoint2 not mounting"
 		exit 1
 	fi
 else
-	logger "PROBLEM: $PROGNAME $mountpoint1 not mounting" && exit 1
+	logger "PROBLEM: $PROGNAME $mountpoint1 not mounting"
+	exit 1
 fi
 
 exit 0
