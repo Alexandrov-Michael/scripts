e15792a212b749a0b19e3f5071c905a12adcfc2f
Alexandrov Michael <mikle.alex@gmail.com>
2016-12-07 15:02:31 +0400

    изменение файлов

--- backup-psql.sh
+++ backup-psql.sh
@@ -5,10 +5,11 @@ NDATE=`date +%Y-%m-%d_%H_%M_%S`
 export PGPASSWORD="Aa12345678"
 bases=('complex')
 IP="172.16.65.35"
+dirD="/home/michael/share/psql/psql"
 
 for b in ${bases[@]}
 do
-	pg_dump -h $IP -p 5432 -U postgres $b | gzip > /home/michael/share/psql/psql/$b.$NDATE.sql.gz
-#	ls -1r $mountdir | grep $b | sed -n '10,$ p' | xargs rm -f {}
+	pg_dump -h $IP -p 5432 -U postgres $b | gzip > $dirD/$b.$NDATE.sql.gz
+	ls -1r $dirD/* | grep $dirD  | grep $b | sed -n '10,$ p' | xargs rm -f {}
 done
 
